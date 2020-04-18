33cfa21652fccfb69a80bd2be66116741da8961d
Michael Kyzyma <mkyzyma@gmail.com>
2020-04-18 11:50:11 +0500

    Coc extensions install script

---
+++ coc.sh
@@ -0,0 +1,1 @@
+ vim -c 'CocInstall -sync coc-json coc-html coc-tsserver coc-svelte coc-eslint coc-calc coc-css coc-cssmodule coc-git coc-highlight coc-python coc-snippets coc-tabnine coc-yank coc-xml|q'
